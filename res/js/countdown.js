e1facf837c84b2693504fcbec67054c9f2635d11
Faheel Ahmad <faheel@live.in>
2017-06-17 11:03:39 +0530

    Move countdown script to a separate file
    and add ASCII art!

---
+++ res/js/countdown.js
@@ -0,0 +1,10 @@
+$('.countdown-timer').countdown({
+    date: "October 03, 2017 09:00:00",
+
+    render: function(data) {
+        $(".days .value").html(this.leadingZeros(data.days, 2));
+        $(".hours .value").html(this.leadingZeros(data.hours, 2));
+        $(".minutes .value").html(this.leadingZeros(data.min, 2));
+        $(".seconds .value").html(this.leadingZeros(data.sec, 2));
+    }
+});
