52ed290f4a7b0cbe3b2b1e5917303c2b1843c35a
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2022-04-20 20:05:29 +0000

    Initial nupdatee

--- res/js/countdown.js
+++ res/js/countdown.js
@@ -1,5 +1,5 @@
 $('.countdown-timer').countdown({
-    date: "October 16, 2017 10:00:00",
+    date: "June 23, 2022 10:00:00",
 
     render: function(data) {
         $(".days .value").html(this.leadingZeros(data.days, 2));
