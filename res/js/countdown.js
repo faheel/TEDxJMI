e79383ddc2d237cad67f2f66a3e424e8fb0516c1
Rishabh Gupta <rishabhgupta@Rishabhs-MacBook-Pro.local>
2022-05-29 01:46:40 +0530

    Events page

--- res/js/countdown.js
+++ res/js/countdown.js
@@ -1,5 +1,5 @@
 $('.countdown-timer').countdown({
-    date: "June 23, 2022 10:00:00",
+    date: "June 12, 2022 10:00:00",
 
     render: function(data) {
         $(".days .value").html(this.leadingZeros(data.days, 2));
